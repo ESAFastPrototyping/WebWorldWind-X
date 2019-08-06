3e9cf7b75705cd7da13fd8bc29a7e769a59a17fc
Jakub Balhar <jakub@balhar.net>
2019-08-06 10:17:17 +0200

    Expose Cloudless Layer

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import LayerOrder from './util/LayerOrder';
 import TexturedSurfacePolygon from './shapes/TexturedSurfacePolygon';
 import TexturedSurfaceShape from './shapes/TexturedSurfaceShape';
 import SciHubProducts from './service/scihub/Products';
+import SentinelCloudlessLayer from "./layer/SentinelCloudlessLayer";
 
 export default {
     Controls: Controls,
     CyclicPickController: CyclicPickController,
     LayerOrder: LayerOrder,
     SciHubProducts: SciHubProducts,
+    SentinelCloudlessLayer: SentinelCloudlessLayer,
     TexturedSurfacePolygon: TexturedSurfacePolygon,
     TexturedSurfaceShape: TexturedSurfaceShape
 };
